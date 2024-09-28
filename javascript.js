b65528fb841bc8062f110364c59987786a593bb2
manju1532005 <manjumm454545@gmail.com>
2024-09-28 10:58:57 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// 1:concatenation;    
+    function concatsrings(string1,string2) {
+    return string1+string2;
+    }
+    console.log(concatsrings("Hey"," messi"));
+    console.log(concatsrings("hello "," ronaldo"));
+    
+// 2:function divide;
+    function divide(num1,num2) {
+    return num1/num2;        
+    }        
+    console.log(divide(20,5));
+    console.log(divide(30,5));
+    
+// 3:calculate Area;
+    function calculateArea(length,width) {
+    return length*width;    
+    }    
+    console.log(calculateArea(4,2));
+    console.log(calculateArea(2,3));
+
+// 4:Arrow function;    
+    const double=(a)=>a*2;
+    console.log(double(2));
+    console.log(double(3));
